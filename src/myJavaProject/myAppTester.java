d6fc02b4a917fb33d0ed9dd21dcbc92a594e95f9
tonny iro <tonnyogange@gmail.com>
2020-06-12 00:57:35 -0700

    commit after creating new method

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -34,14 +34,13 @@ public class myAppTester {
 	   }
 
 
-	   /* 
-	       add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(...){
-
-            
+	   
+	       //add your test method in this space then remove the block comment symbols
+	       public static void myTestMethod(){
+	    	   System.out.println("thanks for using my application");
 
 		   }
 	 
-	   */
+	   
 
 }
